0b1381cc5380799d7a1cf1eb64b9dcab96b4473a
Egor <erura@mentalstack.com>
2018-10-12 17:58:23 +0300

    fix template

--- popup-text.js
+++ popup-text.js
@@ -7,9 +7,8 @@ let mesEror = document.getElementById('mes-error');
 let masData = {
 	'name': '',
 	'group': '',
-	'links': '',
+	'icon': '',
 	'background': '',
-	'choise': '',
 	'title': ''
 };
 
@@ -18,7 +17,6 @@ ajaxTake();  //=====  FULL SELECT
 
 getDataPage();  //=====  TAKE DATA-PAGE
 
-// sendData();
 
 function choice(color) {
 	document.getElementById('background').value = color;
@@ -43,11 +41,15 @@ if (butSend) {
 	butSend.addEventListener('click', function () {
 		
 		masData['name'] = document.getElementById('name').value;
-		masData['group'] = document.getElementById('select').value;
-		masData['links'] = document.getElementById('link').value;
-		masData['background'] = document.getElementById('background').value;
+		masData['name'] = masData['name'].trim();
 		
-		masData['choise'] = document.getElementById('choise_back').checked ? 1 : 0;
+		const $elemSel = document.getElementById('select');
+		masData['group'] = $elemSel.options[$elemSel.selectedIndex].value;
+		
+		masData['icon'] = document.getElementById('icon').value;
+		masData['icon'] = masData['icon'].trim();
+		
+		masData['background'] = document.getElementById('background').value;
 		
 		masData['title'] = document.getElementById('title_text').value;
 		
@@ -66,12 +68,14 @@ function getXhrObject(){
 	return new XMLHttpRequest();
 }
 
+
 //=====  FILL THE SELECT
 function fillSel (data) {
 	let select = document.getElementById('select');
 	
-	data.forEach(function (n) {
+	data['name'].forEach(function (n, i) {
 		let selOpot = document.createElement('option');
+		selOpot.setAttribute('value', data['id'][i]);
 		selOpot.innerHTML = n;
 		if (selOpot && select) select.appendChild(selOpot);
 	});
@@ -83,9 +87,8 @@ function ajaxTake() {
 	var params = [];
 	params.push('directories=true');
 	
-	// var url = 'http://localhost/ajax-ex/add_del_group.php';
-	// var url = 'https://ron17.h19.ru/ex/ajax-ex/add_del_group.php';
-	var url = 'http://ruraeghw.beget.tech/ajax-ex/add_del_group.php';
+	// var url = 'http://ruraeghw.beget.tech/ajax-text/add_del_group.php';
+	var url = 'http://localhost/ajax-text/add_del_group.php';
 	var xhr = getXhrObject();
 	xhr.open('POST', url, true);
 	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
@@ -93,7 +96,7 @@ function ajaxTake() {
 	xhr.onreadystatechange = function() {
 		if(xhr.readyState === 4 && xhr.status === 200){
 			let con = JSON.parse(xhr.responseText);
-			fillSel(con['name']);
+			fillSel(con);
 		}
 	}
 	xhr.send(params.join('&'));
@@ -109,10 +112,8 @@ function ajaxSend() {
 		params.push(key + '=' + masData[key]);
 	}
 	
-	// let url = 'http://localhost/ajax-ex/creatLink.php';
-    // let url = 'http://localhost/ajax-ex/add_del_group.php';
-    // let url = 'http://ron17.h19.ru/ex/ajax-ex/creatLink.php';
-    let url = 'http://ruraeghw.beget.tech/ajax-ex/creatLink.php';
+    // let url = 'http://ruraeghw.beget.tech/ajax-text/creatLink.php';
+    let url = 'http://localhost/ajax-text/creatLink.php';
 	
 	let xhr = getXhrObject();
 	xhr.open('POST', url, true);
@@ -125,36 +126,9 @@ function ajaxSend() {
 				mesEror.style.display = 'block';
 				setTimeout( () => { mesEror.style.display = 'none'; }, 2000);
 			} else {
-				takeScreen();
 				mesSuc.style.display = 'block';
 				setTimeout( () => { mesSuc.style.display = 'none'; }, 2000);
 			}
-		}
-	}
-	xhr.send(params.join('&'));
-}
-
-
-//=====  TAKE SCREEN
-function takeScreen() {
-	
-	let params = [];
-	
-	for (let key in masData) {
-		params.push(key + '=' + masData[key]);
-	}
-	
-	// let urlScreen = 'http://localhost/ajax-ex/screenIcon.php.php';
-	// let urlScreen = 'http://ron17.h19.ru/ex/ajax-ex/screenIcon.php.php';
-	let urlScreen = 'http://ruraeghw.beget.tech/ajax-ex/screenIcon.php.php';
-	
-	let xhr = getXhrObject();
-	xhr.open('POST', urlScreen, true);
-	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	
-	xhr.onreadystatechange = function() {
-		if(xhr.readyState === 4 && xhr.status === 200){
-			let con = JSON.parse(xhr.responseText);
 		}
 	}
 	xhr.send(params.join('&'));
@@ -168,14 +142,35 @@ function getDataPage() {
 		
 		chrome.tabs.executeScript({
 			code: '(' + function() {
-				return {title: window.document.title, url: window.location.href};
+				
+				let sel = ()=>{
+					
+					var range;
+					if (document.selection && document.selection.createRange) {
+						range = document.selection.createRange();
+						return range.htmlText;
+					}
+					else if (window.getSelection) {
+						var selection = window.getSelection();
+						if (selection.rangeCount > 0) {
+							range = selection.getRangeAt(0);
+							var clonedSelection = range.toString();
+							return clonedSelection;
+						}
+						else {
+							return '';
+						}
+					}
+					else {
+						return '';
+					}
+				}
+				
+				
+				return {title: window.document.title, url: window.location.href, text: sel() };
 			} + ')();'
 		}, function(results) {
-			document.getElementById('name').value = results[0]['title'];
-			document.getElementById('name').select();
-			document.getElementById('link').value = results[0]['url'];
+			document.getElementById('name').value = results[0]['text'].trim();
 		});
-		
 	}
-	
 }
